fec18232d75debfe9cfedc1218ab10e3f5fa1ffe
Andrew C. Pacifico <andrewcpacifico@gmail.com>
2016-05-29 19:27:56 -0400

    Using just dist files

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-
-    require('./grunt/workflow.js')(grunt);
-    require('./grunt/sass.js')(grunt);
-    require('./grunt/watch.js')(grunt);
-    require('./grunt/copy.js')(grunt);
-    require('./grunt/uglify.js')(grunt);
-
-};
--- grunt/workflow.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('./package.json')
-    });
-
-    grunt.registerTask('dev', ['sass:dev', 'copy', 'watch']);
-    grunt.registerTask('build', ['sass:dist', 'uglify']);
-};
